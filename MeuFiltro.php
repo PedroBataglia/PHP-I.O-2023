5830137f4d1bfc6ff3350f7035978db0795c57e5
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-11 09:43:09 -0300

    criando um filter

---
+++ MeuFiltro.php
@@ -0,0 +1,32 @@
+<?php
+
+class MeuFilro extends php_user_filter
+{
+    public $stream;
+
+    public function onCreate()
+    {
+        $this->stream = fopen("php://temp", 'w+');
+    }
+
+    public function filter($in, $out, & $consumed, $closing)
+    {
+        $saida = '';
+        while ($bucket = stream_bucket_make_writeable($in)) {
+            $linhas =  explode("\n", $bucket->data);
+
+
+            foreach($linhas as $linha) {
+                if (stripos($linha, 'Parte') !== False) {
+                    $saida .= "$linha\n";
+                }
+            }
+        }
+
+        $bucketSaida = stream_bucket_new($this->stream, $saida);
+        stream_bucket_append($out, $bucketSaida);
+
+        return PSFS_PASS_ON;
+    }
+
+}
