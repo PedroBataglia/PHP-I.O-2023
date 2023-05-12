e8885fea401d2f8a4760e5dbef949acc3e3c6e18
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-12 10:15:15 -0300

    adicionando senha e lendo arquivos

--- httpbin.php
+++ httpbin.php
@@ -3,7 +3,7 @@
 $contexto = stream_context_create([
     'http' => [
         'method' => 'POST',
-        'header' => 'X-From: PHP',
+        'header' => "X-From: PHP\r\nContent-Type: text/plain",
         'content' => 'Teste do corpo da requisição'
     ]
 ]);
--- leitor-zip.php
+++ leitor-zip.php
@@ -1,3 +1,8 @@
 <?php
 
-echo file_get_contents('zip://arquivos.zip#cursos-php.txt');
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => 'ph1234pj56'
+    ]
+]);
+echo file_get_contents('zip://arquivo.zip#cursos-php.txt', false, $contexto);
